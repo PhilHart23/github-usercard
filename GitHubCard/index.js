a841ef191cf3aaf622ef14aab248d69f4297d690
Phil Hart <philhart23@gmail.com>
2019-07-18 19:07:14 -0600

    MVP Complete

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,55 +1,85 @@
-/* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
-*/
+// GET PROFILE
+axios.get(`https://api.github.com/users/PhilHart23`)
+  .then(data => {
+    console.log('Success!', data);
+    const cards = document.querySelector('.cards');
+    cards.appendChild(createCard(data.data));
 
-/* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
+  })
+  .catch(err => {
+    console.log('Error: ', err);
+  })
 
-   Skip to Step 3.
-*/
-
-/* Step 4: Pass the data received from Github into your function, 
-           create a new component and add it to the DOM as a child of .cards
-*/
-
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
-          
-          Using that array, iterate over it, requesting data for each user, creating a new card for each
-          user, and adding that card to the DOM.
-*/
-
+// GET FOLLOWERS
 const followersArray = [];
+axios.get(`https://api.github.com/users/PhilHart23/followers`)
+  .then(data => {
+    console.log('Works! Here is the list of your followers: ', data.data);
+    const followersData = data.data;
+    followersData.forEach(followerData => {
+      followersArray.push(followerData.login);
+    })
+   
 
-/* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
+    followersArray.forEach(follower => {
+      axios.get(`https://api.github.com/users/${follower}`)
+        .then(data => {
+          console.log('Follower info: ', data.data);
+          const cards2 = document.querySelector('.cards');
+          cards2.appendChild(createCard(data.data));
+        })
+        .catch(err => {
+          console.log('Could not retrieve follower info: ', err);
+        })
+    })
 
-<div class="card">
-  <img src={image url of user} />
-  <div class="card-info">
-    <h3 class="name">{users name}</h3>
-    <p class="username">{users user name}</p>
-    <p>Location: {users location}</p>
-    <p>Profile:  
-      <a href={address to users github page}>{address to users github page}</a>
-    </p>
-    <p>Followers: {users followers count}</p>
-    <p>Following: {users following count}</p>
-    <p>Bio: {users bio}</p>
-  </div>
-</div>
+  })
 
-*/
+  .catch(err => {
+    console.log('There was a problem retrieving your list of followers: ', err);
+  })
 
-/* List of LS Instructors Github username's: 
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
-*/
+function createCard(data) {
+  // create the elements
+  const card = document.createElement('div');
+  const userImg = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const name = document.createElement('h3');
+  const username = document.createElement('p');
+  const location = document.createElement('p');
+  const profile = document.createElement('p');
+  const profilePage = document.createElement('a');
+  const followers = document.createElement('p');
+  const following = document.createElement('p');
+  const bio = document.createElement('p');
+
+  // set the styles
+  card.classList.add('card');
+  cardInfo.classList.add('card-info');
+  name.classList.add('name');
+  username.classList.add('username');
+
+  // set the content
+  userImg.src = data.avatar_url;
+  name.textContent = data.name;
+  username.textContent = data.login;
+  location.textContent = `Location: ${data.location}`;
+  profilePage.innerHTML = `Profile: <a href=${data.html_url}>${data.html_url}</a>`;
+  followers.textContent = `Followers: ${data.followers}`;
+  following.textContent = `Following: ${data.following}`;
+  bio.textContent = `Bio: ${data.bio}`;
+
+  // put together
+  card.appendChild(userImg);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(name);
+  cardInfo.appendChild(username);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+  profile.appendChild(profilePage);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+
+  return card;
+}
